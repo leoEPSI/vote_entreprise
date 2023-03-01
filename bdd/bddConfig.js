92b59c52bce3a72d2700342a6ef55ec8d7bb232d
CodingStrqf <l.ruy42@orange.fr>
2023-03-01 14:27:08 +0100

    models, gitignore

--- bdd/bddConfig.js
+++ bdd/bddConfig.js
@@ -7,13 +7,3 @@ var con = mysql.createConnection({
 });
 
 module.exports = con;
-/*
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  con.query("CREATE DATABASE vote", function (err, result) {
-    if (err) throw err;
-    console.log("Database created");
-  });
-});
-*/
