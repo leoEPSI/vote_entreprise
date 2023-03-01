9e8906dc5bc197351a6f773e12cbc312b260fea9
LucasBrobro <brossier.lucas07@gmail.com>
2023-03-01 14:11:05 +0100

    creation tables

--- bdd/bddVote.js
+++ bdd/bddVote.js
@@ -1,16 +1,26 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "yourusername",
-  password: "yourpassword"
+  host: "localhost:3306",
+  user: "root",
+  password: "root"
 });
 
-con.connect(function(err) {
+con.connect(function(err) { 
   if (err) throw err;
   console.log("Connected!");
-  con.query("CREATE DATABASE mydb", function (err, result) {
-    if (err) throw err;
-    console.log("Database created");
-  });
 });
+
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = 'mongodb+srv://admin:admin@voteBDD.mongodb.net/BDDEntreprise?retryWrites=true&w=majority';
+
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log('Connected to MongoDB Atlas');
+//   // Code pour interagir avec la base de données
+// });
