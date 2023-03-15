890500dbf7cc0a053e603ef7d85db8be0b84c999
Asrielbot <49340417+Asrielbot@users.noreply.github.com>
2023-03-15 19:16:07 +0100

    Ajout de types et de requêtes

--- front/src/services/api.ts
+++ front/src/services/api.ts
@@ -1,8 +1,13 @@
-import { addVoteArgs } from "../types";
+import {
+  addProjetArgs,
+  addProjetOutput,
+  addVoteArgs,
+  addVoteOutput,
+} from "../types";
 
 const API_URL = "http://localhost:8080";
 
-export async function addVote(args: addVoteArgs) {
+export async function addVote(args: addVoteArgs): Promise<addVoteOutput> {
   const response = await fetch(`${API_URL}/vote`, {
     method: "PUT",
     headers: {
@@ -15,6 +20,27 @@ export async function addVote(args: addVoteArgs) {
 
 export async function getVote(): Promise<string> {
   const response = await fetch(`${API_URL}/vote`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  return response.json();
+}
+
+export async function addProjet(args: addProjetArgs): Promise<addProjetOutput> {
+  const response = await fetch(`${API_URL}/projet`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(args),
+  });
+  return response.json();
+}
+
+export async function getProjet(): Promise<string> {
+  const response = await fetch(`${API_URL}/projet`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
--- front/src/types.ts
+++ front/src/types.ts
@@ -2,3 +2,24 @@ export interface addVoteArgs {
   valeurVote: boolean;
   date: Date;
 }
+
+export interface addVoteOutput {
+  idVote: string;
+}
+
+export interface addProjetArgs {
+  nomProjet: string;
+  description: string;
+}
+
+export interface addProjetOutput {
+  idProjet: string;
+}
+
+export interface addUtilisateurArgs {
+  nomUtilisateur: string;
+}
+
+export interface addUtilisateurOutput {
+  idUtilisateur: string;
+}
