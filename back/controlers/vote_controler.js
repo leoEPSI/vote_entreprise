c97342cf48f348200bcc60dfcb39aeeda5e91755
CodingStrqf <l.ruy42@orange.fr>
2023-03-15 22:11:13 +0100

    vote

--- back/controlers/vote_controler.js
+++ back/controlers/vote_controler.js
@@ -3,9 +3,13 @@ const vote = require('../models/vote_model');
 class vote_controler{
     async list_vote(request, reponse){
         try {
-const vote = require('../models/vote_model');
-            const votes = await vote.list_vote();
-            
+            const idProjet = request.params.id;
+            const vote_pour = await vote.list_vote_pour(idProjet);
+            const vote_contre = await vote.list_vote_contre(idProjet);
+            let votes = {
+                'valeurPour': vote_pour.length,
+                'valeurContre': vote_contre.length
+            }
             reponse.status(200).send( JSON.stringify(votes));
         } catch (error) {
             reponse.status(500).send({message: "erreur interne, "+error.message});
@@ -31,6 +35,15 @@ const vote = require('../models/vote_model');
             const date = query.date;
             const idUtilisateurVote = query.idUtilisateurVote;
             const idProjetVote = query.idProjetVote;
+
+            // seulement oui ou non
+            if (valeurVote != 1 && valeurVote != 0)
+                return "valeurVote doit être égal à 1 ou 0";
+
+            // vérifier que l'utilisateur n'a pas déjà voté pour ce projet
+            let same_vote = await vote.verify_vote(idUtilisateurVote, idProjetVote);
+            if (same_vote.length > 0)
+                return "Un utilisateur ne peut voter qu'une seule fois pour un projet";
 
             let vote_ = await vote.add_vote(valeurVote, date, idUtilisateurVote, idProjetVote);
             reponse.status(200).send(vote_);
